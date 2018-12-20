1d3aca24b1ea6cf9e2b01d8550f15670dda94aba
andrej.senchov <andrej.senchov@murka.com>
2018-12-20 22:41:34 +0200

    INIT

---
+++ Assets/Scripts/NavMeshBacker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavMeshBacker : MonoBehaviour {
+
+    public MeshFilter Filter;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    [ContextMenu("DO")]
+    public void Do()
+    {
+        Vector3[] vertices = Filter.sharedMesh.vertices;
+    }
+}
---
+++ Assets/Scripts/PlayerMoveController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Scripts
+{
+    class PlayerMoveController : MonoBehaviour
+    {
+        [SerializeField] private NavMeshAgent Agent;
+
+        RaycastHit Hit = new RaycastHit();
+
+        public void Update()
+        {
+            if (Input.GetMouseButtonDown(0) && !Input.GetKey(KeyCode.LeftShift))
+            {
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray.origin, ray.direction, out Hit))
+                    Move(Hit.point);
+                NavMeshTriangulation navMeshTriangulation = new NavMeshTriangulation();
+            }
+        }
+
+            private void Move(Vector3 dest)
+        {
+            Agent.SetDestination(dest);
+        }
+    }
+}
